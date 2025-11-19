a18bf7a7abf7803733e443a72f85e70123803fb6
BinateWizard <aezravito12@gmail.com>
2025-11-19 11:27:47 +0800

    feat: implement FireTapLoader component for improved loading experience and update App.vue for conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <div id="app">
-    <router-view />
-    <InstallPrompt />
+    <FireTapLoader v-if="isRouteLoading" />
+    <div v-else>
+      <router-view />
+      <InstallPrompt />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import InstallPrompt from '@/components/InstallPrompt.vue'
+import FireTapLoader from '@/components/FireTapLoader.vue'
+
+const isRouteLoading = ref(true)
+const router = useRouter()
+
+let firstLoad = true
+
+router.isReady().then(() => {
+  if (firstLoad) {
+    // Ensure loader shows briefly on first load even if fast
+    setTimeout(() => {
+      isRouteLoading.value = false
+      firstLoad = false
+    }, 1200) // slightly shorter than previous 3000ms for snappier feel
+  } else {
+    isRouteLoading.value = false
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  isRouteLoading.value = true
+  next()
+})
+
+router.afterEach(() => {
+  // Small delay allows view mount transitions to settle
+  setTimeout(() => {
+    isRouteLoading.value = false
+  }, 250)
+})
 </script>
 
